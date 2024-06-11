503296d3def01e70ee6fce236f329337a190bca4
LooMeN599 <dice11011991@gmail.com>
2024-06-11 20:04:04 +0900

    v41

--- script.js
+++ script.js
@@ -1,69 +1,20 @@
-// 選択されたボタンの状態を保持する変数
-var selectedButtons = {
-    'A': null,
-    'B': null,
-    'C': null,
-    'D': null,
-    'E': null,
-    'F': null
-};
-
-// ボタンのクリック時の処理
-document.querySelectorAll('.num-set-button-A').forEach(function(button) {
-    button.addEventListener('click', function() {
-        toggleSelection('A', button);
+document.addEventListener("DOMContentLoaded", function() {
+    // ボタンのクリック時の処理を追加
+    document.querySelectorAll('.num-set-button').forEach(function(button) {
+        button.addEventListener('click', function() {
+            toggleSelection(button);
+        });
     });
 });
 
-document.querySelectorAll('.num-set-button-B').forEach(function(button) {
-    button.addEventListener('click', function() {
-        toggleSelection('B', button);
-    });
-});
-
-document.querySelectorAll('.num-set-button-C').forEach(function(button) {
-    button.addEventListener('click', function() {
-        toggleSelection('C', button);
-    });
-});
-
-document.querySelectorAll('.num-set-button-D').forEach(function(button) {
-    button.addEventListener('click', function() {
-        toggleSelection('D', button);
-    });
-});
-
-document.querySelectorAll('.num-set-button-E').forEach(function(button) {
-    button.addEventListener('click', function() {
-        toggleSelection('E', button);
-    });
-});
-
-document.querySelectorAll('.num-set-button-F').forEach(function(button) {
-    button.addEventListener('click', function() {
-        toggleSelection('F', button);
-    });
-});
-
-// 選択されたボタンをハイライトする処理
-function highlightSelectedButtons() {
-    Object.keys(selectedButtons).forEach(function(slot) {
-        var selectedButton = selectedButtons[slot];
-        if (selectedButton !== null) {
-            var allButtons = document.querySelectorAll('.num-set-button-' + slot);
-            allButtons.forEach(function(button) {
-                button.classList.remove('selected');
-            });
-            selectedButton.classList.add('selected');
-        }
-    });
+// ボタンの選択状態を切り替える関数
+function toggleSelection(button) {
+    button.classList.toggle('selected');
 }
 
 // 入力クリアボタンのクリック時の処理
 function clearSelection() {
-    Object.keys(selectedButtons).forEach(function(slot) {
-        clearSelectedButton(slot);
+    document.querySelectorAll('.num-set-button').forEach(function(button) {
+        button.classList.remove('selected');
     });
-    // 選択状況をクリアしたので、ハイライトもクリア
-    highlightSelectedButtons();
 }
